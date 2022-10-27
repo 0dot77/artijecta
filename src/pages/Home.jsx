f1bd9bb696c7c4de2ca09bb3d48fe812208fa470
0dot77 <yty0706@gmail.com>
2022-10-27 12:50:56 +0200

    glitch test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import { Canvas } from '@react-three/fiber';
 import styled from 'styled-components';
 import HomeModel from '../components/HomeModel';
 import { Loader, OrbitControls } from '@react-three/drei';
+import { EffectComposer } from '@react-three/postprocessing';
+import { Glitch } from '@react-three/postprocessing';
+import { GlitchMode } from 'postprocessing';
 
 const HomeContainer = styled.main`
   width: 100%;
@@ -33,6 +36,17 @@ export default function Home() {
         />
         <ambientLight />
         <HomeModel />
+        <EffectComposer>
+          <Glitch
+            delay={[1.5, 3.5]} // min and max glitch delay
+            duration={[0.5, 1]} // min and max glitch duration
+            strength={[1, 2]} // min and max glitch strength
+            mode={GlitchMode.SPORADIC} // glitch mode
+            active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
+            ratio={0} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+            dtSize={5}
+          />
+        </EffectComposer>
       </Canvas>
     </HomeContainer>
   );
